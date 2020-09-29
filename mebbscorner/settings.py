e0fa5191e9280f78ec6b2a91f0f5e4b5ca3da00c
triple07even <57076842+triple07even@users.noreply.github.com>
2020-09-30 00:20:52 +0100

    'setting.py_mod'

--- mebbscorner/settings.py
+++ mebbscorner/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '!_a%%fk9=z=buj(#&q50rlgqeygp2jqsr&#m&hczbc7$pmu6%7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
