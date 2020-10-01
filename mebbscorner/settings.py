83e55becebb5fd339e07e5781735bcbf7730f472
triple07even <57076842+triple07even@users.noreply.github.com>
2020-10-01 01:16:36 +0100

    'require0.2'

--- mebbscorner/settings.py
+++ mebbscorner/settings.py
@@ -154,7 +154,7 @@ AWS_STORAGE_BUCKET_NAME = 'mebbscorner001'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
